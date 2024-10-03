b4126cb62cd7ff12fd97688b8a03986cb790496b
p-gntr <121146258+p-gntr@users.noreply.github.com>
2024-10-03 16:00:54 -0400

    rough draft of smoothness metric

---
+++ Graphs/metric2.py
@@ -0,0 +1,16 @@
+from metric1 import m1, q
+import math
+
+
+
+def c(z):
+    T = 20;
+    k_0 = 0;
+    c = 0
+    for k in range(k_0, k_0+ T - 1):
+        c += abs(z[k+1] - z[k])
+
+    return c/T 
+
+def m2(z):
+    return m1(z)/c(z)
