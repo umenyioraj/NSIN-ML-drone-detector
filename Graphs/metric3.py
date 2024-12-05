fb3bfe787a85cba1ea68446e2debeb1d7b50f933
natalieb27 <nbohmbac@villanova.edu>
2024-12-05 14:20:30 -0500

    Changed M3 Metric

--- Graphs/metric3.py
+++ Graphs/metric3.py
@@ -1,6 +1,6 @@
 from metric1 import m1
 from metric2 import m2
 
-def m3(z, k_0, T):
+def m3(z, k_0, T,rate):
     p = 1; # where p is some constant
-    return min(m1(z, k_0, T), 0.5*m2(z, k_0, T))
+    return min(m1(z, k_0, T,rate), 0.5*m2(z, k_0, T,rate))
