62839f5606c53fd1def420f7878eef5ae4b82b9a
graydonspaul <graydonspaul@gmail.com>
2025-01-23 13:59:01 -0500

    uploadbtn

--- GUI/initialGUI.py
+++ GUI/initialGUI.py
@@ -1,5 +1,6 @@
 # import tkinter 
 from tkinter import * 
+from tkinter import filedialog
 
 # create a root window 
 root = Tk() 
@@ -8,6 +9,36 @@ root = Tk()
 root.title("Drone Detection") 
 # geometry is width x height 
 root.geometry('350x200')
+
+# enter in a file that we can run our algorithms on 
+# create function to open your files and select 
+
+
+def open_file_dialog():
+    file_path = filedialog.askopenfilename(title="Select a File", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
+    if file_path:
+        selected_file_label.config(text=f"Selected File: {file_path}")
+        process_file(file_path)
+
+def process_file(file_path):
+    # Implement your file processing logic here
+    # For demonstration, let's just display the contents of the selected file
+    try:
+        with open(file_path, 'r') as file:
+            file_contents = file.read()
+            file_text.delete('1.0', END)
+            file_text.insert(END, file_contents)
+    except Exception as e:
+        selected_file_label.config(text=f"Error: {str(e)}")
+
+selected_file_label = Label(root, text="Selected File:", height = 50, width = 100)
+
+file_text = Text(root, height=10, width=40)
+
+
+# create upload button 
+uploadBtn = Button(root, text = "Upload Your File", fg = "black", command = open_file_dialog)
+uploadBtn.grid(column = 2, row = 0)
 
 # add a menu bar in the root window 
 menu = Menu(root)
@@ -19,29 +50,30 @@ menu.add_cascade(label='File', menu=item)
 root.config(menu=menu)
 
 # create text in the root window
-lbl = Label(root, text = "Enter the ID")
-lbl.grid()
+# lbl = Label(root, text = "Enter the ID")
+# lbl.grid()
 
 # create an entry field 
-txt = Entry(root, width=10)
-txt.grid(column=1, row=0)
+# txt = Entry(root, width=10)
+# txt.grid(column=1, row=0)
 
+###
 # what happens when the button gets clicked 
-def clicked(): 
+# def clicked(): 
     # allows for user entry to be shown 
-    res = "The ID you are looking for is " + txt.get()
-    lbl.configure(text = res)
+    # res = "The ID you are looking for is " + txt.get()
+    # lbl.configure(text = res)
 
     # changes the text when you click to be "surprise!"
     # lbl.configure(text = "surprise!")
 
 
 # create the button 
-btn = Button(root, text = "Enter", fg = "black", command = clicked)
+# btn = Button(root, text = "Enter", fg = "black", command = clicked)
 
 # put the button in the root window but you can change where it is 
 # tkinter uses row and columnns for placements 
-btn.grid(column = 2, row = 0)
+# btn.grid(column = 2, row = 0)
 
 
 root.mainloop()
